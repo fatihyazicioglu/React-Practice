390e342339f92c52fbb79f58219bc9d6bd5c07da
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-07-04 23:52:04 +0200

    className, htmlFor, {},ternaries

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 
 function App() {
+  const test =123;
+  const isAuth =true ;
   return (
     <div className="App">
    <h1>Hello</h1>
+   <label>not Class but ClassName.</label>
+   <h1>not for  but Htmlfor(label)</h1>
+
+   <h1>there must be only 1 parent element.This h1 cannot be outside of div</h1>
+    {/* for operations {} */}
+    <h1>    {1+1} </h1>
+    <h1>{"fatih".toUpperCase()}</h1>    
+    <h1>{test}</h1>
+    
+    <div>
+      {
+        isAuth ? <p>it exists</p> : null
+      }
     </div>
+
+
+
+    </div>
+       
+
   );
 }
 
